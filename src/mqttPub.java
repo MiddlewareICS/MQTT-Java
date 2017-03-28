88aaab7e9d74dec612f96bd3705f6b5516dccbbb
harvey <heweimr@gmail.com>
2017-03-28 22:43:13 +0800

    first-pub sub可以分别运行，同时运行会冲突

---
+++ src/mqttPub.java
@@ -0,0 +1,39 @@
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+
+public class mqttPub {
+
+  MqttClient client;
+  
+  public mqttPub() {}
+
+  public static void main(String[] args) {
+    new mqttPub().doDemo();
+  }
+
+  public void doDemo() {
+    try {
+      client = new MqttClient("tcp://127.0.0.1:1883", "pahomqttPublish1");
+      client.connect();
+      MqttMessage message = new MqttMessage();
+      message.setPayload("A single message".getBytes());
+      
+      while(true){
+    	  client.publish("TEST", message);
+    	  try{
+    		  	Thread.sleep(1000);//1000ms = 1sec
+    	  }catch(InterruptedException e){
+    			System.err.println("Interrupted");
+    	  }
+      }     
+//      client.disconnect();   
+//      System.out.println("Hello");
+    
+    } catch (MqttException e) {
+      e.printStackTrace();
+      System.out.println("World");
+    }
+  }
+}
